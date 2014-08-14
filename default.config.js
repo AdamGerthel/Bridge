82975bdacecb72eab263025e800907b687b1c265
Adam Gerthel <adam.gerthel@oddhill.se>
2014-08-14 16:24:55 +0200

    Improved documentation

--- default.config.js
+++ default.config.js
@@ -1,7 +1,11 @@
 /*
  * Copy this file, name it config.js and enter your configuration.
- * remoteCouch is only need if you're using a remote CouchDB instance.
+ * remoteCouch is only needed if you're using a remote CouchDB instance,
+ * which is needed for synchronization across devices/clients.
  * If you're only using the app locally, you can leave remoteCouch empty.
+ *
+ * The local database (i.e PouchDB) can be called anything, but 'docs' or
+ * 'bridge' is recommended.
  *
  * Example configuration:
  * var db = new PouchDB('docs');
